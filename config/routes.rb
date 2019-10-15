be52d23333bba657c4c38c33acdde97d96caf46d
github username <github email address>
2019-10-15 16:48:26 -0700

    creating controllers for account and host

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get 'hosts/new'
+  get 'hosts/create'
+  get 'hosts/edit'
+  get 'hosts/update'
+  get 'hosts/delete'
+  get 'accounts/new'
+  get 'accounts/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
