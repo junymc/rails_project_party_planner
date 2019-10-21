4133f1e31c769806543d99da11fc02db7396e536
github username <github email address>
2019-10-21 14:56:06 -0700

    fixing invites controller(not working)

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,13 +1,18 @@
 class InvitesController < ApplicationController
+
   def new
     @invite = Invite.new
+    @party_id = params[:party_id]
+    
   end
 
   def create
     @invite = Invite.new(invite_params)
+    # binding.pry
     if @invite.save
-       redirect_to guest_invites_path
+       redirect_to guest_invites_path(current_user)
     else
+      @party_id= params[:party_id]
       render :new
     end
   end
@@ -15,6 +20,7 @@ class InvitesController < ApplicationController
   def index
     if params[:guest_id]
       @invites = Guest.find(params[:guest_id]).invites
+      
     else
       @invites = Invite.all
     end
--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,5 +2,6 @@ class Invite < ApplicationRecord
     belongs_to :party
     belongs_to :guest
 
-    validates :add_on, rsvp, presence: true
+    validates :add_on, :rsvp, presence: true
+    # accepts_nested_attributes_for :guest, :party
 end
