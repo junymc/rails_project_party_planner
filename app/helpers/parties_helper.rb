7cff52c03ad6c3f0e8644b2350a67748cce8bcf2
github username <github email address>
2019-10-25 11:35:59 -0700

    giving the link a condition for certain guest

--- app/helpers/parties_helper.rb
+++ app/helpers/parties_helper.rb
@@ -1,6 +1,3 @@
 module PartiesHelper
-    def already_joined
-        @party = Party.find(params[:id])
-        @invite = Invite.where("guest_id = ? OR party_id = ?", current_user, @party.id)
-      end
+
 end
