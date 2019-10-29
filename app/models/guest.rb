51a860cdefa0245fb8c9486fab48a8763250fc46
github username <github email address>
2019-10-29 10:00:58 -0700

    adding scope and guests count

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -4,5 +4,8 @@ class Guest < ApplicationRecord
     has_many :parties, through: :invites
 
     validates :name, :age, presence: true
-    validates :age, numericality: { greater_than: 20, less_than: 90}
+    validates :age, numericality: { greater_than: 19, less_than: 90}
+
+    scope :female, -> { where(gender: :female) }
+    scope :female_and_adult, -> { female.where("age > 20") }
 end
