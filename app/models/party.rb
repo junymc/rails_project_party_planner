857db7b2a5c48d780c1014b471ae6e2b0ac6decd
github username <github email address>
2019-11-01 11:01:39 -0700

    adding some more validation

--- app/models/party.rb
+++ app/models/party.rb
@@ -3,6 +3,8 @@ class Party < ApplicationRecord
     has_many :invites
     has_many :guests, through: :invites
 
+    validates :name, :description, :location, :date_time, presence: true
+
     def strf_time
         date_time.strftime("%d %b %Y %l:%M %p")
     end
